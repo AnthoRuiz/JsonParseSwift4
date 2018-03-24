a271c4f4ac82267ad1edc1880ce90e4ba78341de
Anthony Ruiz <yobakaj@gmail.com>
2018-03-24 09:24:10 -0400

    Example API REST with Json Data using Swift4

--- JsonParseSwift4/ViewController.swift
+++ JsonParseSwift4/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
+struct WebsiteDescription: Decodable {
+   let name: String
+   let description: String
+   let courses: [Course]
+}
+
+struct Course: Decodable {
+   let id: Int
+   let name: String
+   let link: String
+   let imageUrl: String
+}
+
 class ViewController: UIViewController {
 
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
+      
+      let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+      guard let url = URL(string: jsonUrlString) else{
+         return
+      }
+      
+      URLSession.shared.dataTask(with: url) { (data, error, err) in
+      
+         guard let data =  data else {return}
+         
+         do{
+            
+            let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+            print(websiteDescription.name)
+            for json in websiteDescription.courses  {
+               print(json.name)
+            }
+            
+//            let courses = try JSONDecoder().decode([Course].self, from: data)
+//            print(courses)
+            
+         }catch let jsonErr{
+            print("error", jsonErr)
+         }
+         
+      }.resume()
    }
 
    override func didReceiveMemoryWarning() {
       super.didReceiveMemoryWarning()
       // Dispose of any resources that can be recreated.
    }
-
-
 }
 
